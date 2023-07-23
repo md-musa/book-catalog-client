c2c98d175121ab62a9f22cb595584682580acc1d
md-musa <mohammad.musa706@gmail.com>
2023-07-23 20:34:27 +0600

    Code improved

--- src/pages/AllBooks.tsx
+++ src/pages/AllBooks.tsx
@@ -1,23 +1,23 @@
-import React, { useState } from 'react';
+import { useState, ChangeEvent, FormEvent } from 'react';
 import BookCard from '../components/BookCard';
-import { Button, Input, Option, Select } from '@material-tailwind/react';
-import { useGetBooksQuery } from '../store/features/books/bookApiSlice';
+import { Button, Input } from '@material-tailwind/react';
+import { useGetBooksQuery, Book } from '../store/features/books/bookApiSlice';
 
 interface IParams {
-  searchTerm: string;
-  genre: string;
-  publicationYear: string;
+  searchTerm?: string;
+  genre?: string;
+  publicationYear?: string;
 }
 
 function AllBooks() {
   const [searchTerm, setSearchTerm] = useState<string>('');
   const [genre, setGenre] = useState<string>('');
-  const [publicationYear, setPublicationYear] = useState<string>();
+  const [publicationYear, setPublicationYear] = useState<string>('');
 
   const [genreValue, setGenreValue] = useState<string>('');
-  const [date, setDate] = useState<number>();
+  const [date, setDate] = useState<string>('');
 
-  const params: Partial<IParams> = {};
+  const params: IParams = {};
   if (searchTerm) params.searchTerm = searchTerm;
   if (genre) params.genre = genre;
   if (publicationYear) params.publicationYear = publicationYear;
@@ -27,7 +27,8 @@ function AllBooks() {
 
   const books = data?.data;
 
-  function setFilteringValues() {
+  function setFilteringValues(e: FormEvent<HTMLButtonElement>) {
+    e.preventDefault();
     setGenre(genreValue);
     setPublicationYear(date);
   }
@@ -39,14 +40,18 @@ function AllBooks() {
       <div className="border-r-2 py-20 px-4 space-y-4">
         <p className="text-gray-800">Filter books</p>
         <div>
-          <Input value={genreValue} onChange={e => setGenreValue(e.target.value)} label="Genre" />
+          <Input
+            value={genreValue}
+            onChange={(e: ChangeEvent<HTMLInputElement>) => setGenreValue(e.target.value)}
+            label="Genre"
+          />
         </div>
 
         <div>
           <Input
             type="number"
             value={date}
-            onChange={e => setDate(e.target.value)}
+            onChange={(e: ChangeEvent<HTMLInputElement>) => setDate(e.target.value)}
             label="Publication Year"
           />
         </div>
@@ -58,28 +63,17 @@ function AllBooks() {
       <div className="w-full">
         <div className="mt-4 flex justify-center">
           <div className="">
-            {/* <Input
-              value={searchTerm}
-              onChange={e => setSearchTerm(e.target.value)}
-              type="text"
-              className="h-[50px] w-[30rem] rounded-full text-lg focus:!border-t-blue-500 focus:!border-blue-500 ring-4 ring-transparent focus:ring-blue-500/20 !border-2 !border-blue-gray-50 bg-white shadow-lg shadow-blue-gray-900/5 placeholder:text-blue-gray-200 text-blue-gray-500"
-              className="h-[50px] w-[30rem] rounded-full text-lg focus:!border-t-blue-500 focus:!border-blue-500 ring-4 ring-transparent focus:ring-blue-500/20 !border-2 !border-blue-gray-50 bg-white shadow-lg shadow-blue-gray-900/5 placeholder:text-blue-gray-200 text-blue-gray-500"
-              labelProps={{
-                className: 'hidden',
-              }}
-              containerProps={{ className: 'min-w-[100px]' }}
-            /> */}
             <input
               type="text"
               className="px-4 outline-none h-[50px] w-[30rem] rounded-full text-lg focus:!border-t-blue-500 focus:!border-blue-500 ring-4 ring-transparent focus:ring-blue-500/20 !border-2 !border-blue-gray-50 bg-white shadow-lg shadow-blue-gray-900/5 placeholder:text-blue-gray-200 text-blue-gray-500"
               placeholder="Search books"
               value={searchTerm}
-              onChange={e => setSearchTerm(e.target.value)}
+              onChange={(e: ChangeEvent<HTMLInputElement>) => setSearchTerm(e.target.value)}
             />
           </div>
         </div>
         <div className="my-10 grid grid-cols-4 gap-2">
-          {books?.map(book => (
+          {books?.map((book: Book) => (
             <BookCard book={book} key={book._id} />
           ))}
         </div>
